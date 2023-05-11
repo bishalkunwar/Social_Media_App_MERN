8de08e09d15ecc4f7cc394a15cb8477f30e4fd58
bishal <bishal.kunwar487@gmail.com>
2023-05-10 22:10:29 -0400

    Update and Delete Function has bugs, CRUD and Like featur Done

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import {Card, CardActions, CardContent, CardMedia, Button, Typography} from "@material-ui/core";
-import ThumbAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import {useDispatch} from "react-redux";
 
 import useStyles from "./styles";
+import {likePost, deletePost} from "../../../actions/posts";
 
-
-const Post = () => {
+const Post = ({post, setCurrrentId}) => {
 
   const dispatch = useDispatch();  // dispatch will do the data loading dispatching.
   const classes = useStyles();
 
   return (
-   <Card>
-    <CardMedia>
+   <Card className={classes.card}>
+    <CardMedia className={classes.media} image={post.selectedFile || 'https://github.com/settings/profile'} title={post.title}/>
+      <div className={classes.overlay}>
+        <Typography variant='h6'>{post.creator}</Typography>
+        <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
+      </div>
 
-    </CardMedia>
+      <div className={classes.overlay2}>
+        <Button style={{color: 'white'}} size='small' onClick={()=>setCurrrentId(post._id)}><MoreHorizIcon fontSize='default'/></Button>
+      </div>
 
+      <div className={classes.details}>
+        <Typography variant='body2' color='textSecondary' component='h2'>{post.tags.map((tag)=>`#${tag}`)}</Typography>
+      </div>
+      <Typography className={classes.title} gutterBottom variant='h5' component="h2">{post.title}</Typography>
     <CardContent>
-
+      <Typography variant='body2' color='textSecondary' component="p">{post.message}</Typography>
     </CardContent>
 
-    <CardActions>
-      
+    <CardActions className={classes.CardActions}>
+        <Button size='small' color='primary' onClick={()=>dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small"/>Like{post.likeCount}</Button>
+        <Button size='small' color='primary' onClick={()=> dispatch(deletePost(post._id))}><DeleteIcon fontSize='small'/>Delete</Button>
     </CardActions>
    </Card>
     
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,7 +9,6 @@ export const getPosts = async (req, res) =>{
         const postMessages = await PostMessage.find();
 
         res.status(200).json(postMessages);
-        console.log("switched until here");
     } catch (error) {
         res.status(404).json({message: error.message});   
         //The HTTP 404 Not Found response status code indicates that the server cannot find the requested resource
@@ -21,7 +20,7 @@ export const getPost = async(req, res)=>{
     const {id}= req.params;
     
     try {
-        const post = await postMessage.findById(id);
+        const post = await PostMessage.findById(id);
         res.status(200).json(post);    
     } catch (error) {
         res.status(404).json({message: error.message});
@@ -37,7 +36,7 @@ export const createPost = async (req, res) => {
         console.log("pressed upto here");
        await newPostMessage.save();
        res.status(201).json(newPostMessage);
-       console.log("writing upto here");
+       console.log("writing upto here23");
     } catch (error) {
         res.status(409).json({message: error.message});
         // The HTTP 409 status code (Conflict) indicates that the request could not be processed because of conflict in the request
@@ -49,26 +48,28 @@ export const updatePost = async(req, res) => {
     const {id} = req.params;
     const {title, message, selectedFile, creator, tags} = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(404).send(`No post with id: ${id}`);
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
     const updatedPost = {creator, title, message, tags, selectedFile, _id:id};
 
-    await PostMessage.findByIdAndUpdate(id, updatePost, {new: true});
+    await PostMessage.findByIdAndUpdate(id, updatedPost, {new: true});
 
     res.json(updatedPost);
+    console.log("Update is calling");
 
 };
 
 
 export const deletePost = async(req, res)=>{
 
-    const {id} = req.params;
+    const { id } = req.params;
+    res.send("calling the delete");
+    console.log("Calling the delete");
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with such id: ${id}`);
+    await PostMessage.findByIdAndRemove(id);
 
-    const post = await PostMessage.findByIdAndRemove(id);
-
-    res.json({message: "Post Deleted Successfully"});
+    res.json({ message: "Post deleted successfully." });
 };
 
 export const likePost = async(req, res) => {
@@ -77,9 +78,10 @@ export const likePost = async(req, res) => {
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with such id: ${id}`);
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findIdAndUpdate(id, {likeCount: post.likeCount+1});
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount+1});
 
     res.json(updatedPost);
+    console.log("Liked though");
 };
 
 export default router;
