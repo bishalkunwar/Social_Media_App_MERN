6164ff4c29605c661e50cff276d06287b8e6fea0
bishal <bishal.kunwar487@gmail.com>
2023-05-25 19:33:45 -0400

    OAuth working at backend, manual auth actionreducers left

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,16 +1,18 @@
 import React,{ useState} from 'react';
 import {Avatar, Button, Paper, Grid, Typography, Container} from "@material-ui/core";
+import {GoogleLogin} from 'react-google-login';
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-// import {useDispatch} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import useStyles from "./styles";
 import Input from './Input';
+import Icon from "./icon";
 
 // const initialState = {firstName: '', lastName: '', email: '', password: '', confirmPassword: ''};
 
 const Auth = () => {
 
     const classes = useStyles();
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const [showPassword, setShowPassword] = useState(false);
     // // const isSignUp =true;
     const [isSignUp, setIsSignUp] = useState(true);
@@ -32,6 +34,20 @@ const Auth = () => {
         setShowPassword(false);
 
     };
+
+    const googleSuccess = async(req, res) => {
+        const result = res?.profileObj;
+        const token = res?.tokenId;
+
+        try {
+            dispatch({ type: 'AUTH', data: {result, token} });
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    const googleError = () => alert('Google Sign In was unsuccessful, you can try again later');
 
     
     return (
@@ -55,9 +71,23 @@ const Auth = () => {
                         { isSignUp && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
                     </Grid>
 
+
                     <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
                         {isSignUp?"Sign Up":"Sign In"}
                     </Button>
+
+                    <GoogleLogin 
+                        clientId='290140167547-f67k59iugfv5aeo05qv9k71808sb93oj.apps.googleusercontent.com'
+
+                        render = {(renderProps)=>(
+                            <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained"> 
+                                Google SignIn
+                            </Button>
+                        )}
+
+                        onSuccess={googleSuccess} onFailure={googleError} cookiePolicy='single_host_origin'
+                    />
+
                     <Grid container justify='flex-end'>
                             <Grid item>
                                 <Button onClick={switchMode}>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,5 +3,6 @@
 import { combineReducers } from "redux";
 
 import Posts from "./posts";
+import auth from "./auth";
 
-export const reducers = combineReducers({Posts});
+export const reducers = combineReducers({Posts, auth});
