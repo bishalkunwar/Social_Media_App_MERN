92cabe449858b833d6a48b182c7551726c90c0e6
bishal <bishal.kunwar487@gmail.com>
2023-06-17 14:17:12 -0400

    push with pagination

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect}from 'react';
-import {Container, Grow, Grid } from "@material-ui/core";
+import {Container, Grow, Grid, Paper } from "@material-ui/core";
 import { useDispatch } from 'react-redux';
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 // import useStyles from './styles';
 import {getPosts} from "../../actions/posts";
+import Pagination from '../Pagination';
 
 // useDispatch() ​: This hook returns a reference to the dispatch function from the Redux store
 // useEffect() : By using this Hook, you tell React that your component needs to do something after render.
@@ -30,6 +31,9 @@ const Home = () => {
             </Grid>
             <Grid item xs={12} sm={4}>
                 <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                <Paper elevation={6}>
+                  <Pagination />
+                </Paper>
             </Grid>
           </Grid>
         </Container>  
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -20,4 +20,8 @@ export default makeStyles(() => ({
         marginLeft: '15px',
     },
     
+    pagination: {
+
+    },
+    
 })); 
